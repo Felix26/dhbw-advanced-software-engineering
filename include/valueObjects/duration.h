395c6d90df50d6926d549441c54a19be52740995
Felix Miller <Felixmiller2005@outlook.de>
2026-04-24 13:12:53 +0200

    refactoring pt2: enable calculating with durations

--- include/valueObjects/duration.h
+++ include/valueObjects/duration.h
@@ -10,6 +10,16 @@ class Duration
 
         int getDurationInMinutes() const;
 
+        Duration operator+(const Duration& other) const;
+        Duration &operator+=(const Duration& other);
+        Duration operator-(const Duration& other) const;
+        Duration &operator-=(const Duration& other);
+
+        Duration operator*(double factor) const;
+        Duration &operator*=(double factor);
+        Duration operator/(double divisor) const;
+        Duration &operator/=(double divisor);
+
         friend std::ostream& operator<<(std::ostream& os, const Duration& duration);
 
     private:
--- src/valueObjects/duration.cpp
+++ src/valueObjects/duration.cpp
@@ -12,6 +12,51 @@ int Duration::getDurationInMinutes() const
     return mDuration.count();
 }
 
+
+Duration Duration::operator+(const Duration &other) const
+{
+    return Duration(this->mDuration + other.mDuration);
+}
+
+Duration &Duration::operator+=(const Duration &other)
+{
+    this->mDuration += other.mDuration;
+    return *this;
+}
+
+Duration Duration::operator-(const Duration &other) const
+{
+    return Duration(this->mDuration - other.mDuration);
+}
+
+Duration &Duration::operator-=(const Duration &other)
+{
+    this->mDuration -= other.mDuration;
+    return *this;
+}
+
+Duration Duration::operator*(double factor) const
+{
+    return Duration(std::chrono::minutes(static_cast<int>(this->mDuration.count() * factor)));
+}
+
+Duration &Duration::operator*=(double factor)
+{
+    this->mDuration = std::chrono::minutes(static_cast<int>(this->mDuration.count() * factor));
+    return *this;
+}
+
+Duration Duration::operator/(double divisor) const
+{
+    return Duration(std::chrono::minutes(static_cast<int>(this->mDuration.count() / divisor)));
+}
+
+Duration &Duration::operator/=(double divisor)
+{
+    this->mDuration = std::chrono::minutes(static_cast<int>(this->mDuration.count() / divisor));
+    return *this;
+}
+
 std::ostream &operator<<(std::ostream &os, const Duration &duration)
 {
     int totalMinutes = duration.getDurationInMinutes();
