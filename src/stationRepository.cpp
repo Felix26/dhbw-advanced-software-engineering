2b4a95d01afbb445971d391185e7e3fad90cbcae
Felix Miller <Felixmiller2005@outlook.de>
2026-04-28 14:58:05 +0200

    more formatting rules

--- src/stationRepository.cpp
+++ src/stationRepository.cpp
@@ -32,5 +32,26 @@ std::string StationRepository::formatStationName(const ZHVData &data) const
         return data.name;
     }
 
+    /*
+    e.g. for the entry:
+    de:08316:11220,Sasbach Winzergenossenschaft,"48,137965","7,613581",Sasbach am Kaiserstuhl
+    Should become:
+    Sasbach am Kaiserstuhl Winzergenossenschaft
+    */
+    size_t firstSpacePos = data.name.find(' ');
+    if (firstSpacePos != std::string::npos)
+    {
+        // Isolate the first word of the station name, which is often the same as the municipality name (e.g., "Sasbach" in "Sasbach Winzergenossenschaft").
+        std::string firstWord = data.name.substr(0, firstSpacePos);
+        
+        // does the municipality start with eg "Sasbach "? 
+        // (using "Sasbach "(!) with space, to differentiate between eg "Sasbach" and "Sasbachwalden")
+        if (data.municipality.starts_with(firstWord + " "))
+        {
+            // combine: "Sasbach am Kaiserstuhl" + " Winzergenossenschaft"
+            return data.municipality + data.name.substr(firstSpacePos);
+        }
+    }
+
     return std::format("{} {}", data.municipality, data.name);
 }
